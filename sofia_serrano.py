85527207ab74219e7e6d6ce40599dbc357e6dffd
sofia_serrano <sofiaserrano499@gmail.com>
2021-10-06 18:47:10 -0300

    Ejercicio hecho aunque siempre se puede mejorar

---
+++ sofia_serrano.py
@@ -0,0 +1,38 @@
+def comparar_palabras(pal,pal2):
+    #me devuelve a partir de donde son distintas o donde esta su ultima letra 
+    pos=0
+    band=False
+    i=0
+    while i<len(pal) and i<len(pal2) and band==False:
+        if pal[i]!=pal2[i]:
+            band=True
+        i=1+i
+    pos=i-1
+    return pos
+
+def encontrarmayor(lst,stri):
+    may=lst[0]
+    for i in range(0,len(lst)):
+        comp=comparar_palabras(lst[i],may)
+        if stri.index(lst[i][comp])<stri.index(may[comp]):
+            may=lst[i]
+        if stri.index(lst[i][comp])<=stri.index(may[comp]) and len(lst[i])<len(may):
+            may=lst[i]
+    return may
+
+def ordenar_extraterrestre(desordenadas,orden_alfabeto):
+    ordenada=[]
+    while len(desordenadas)>0:
+        agregar=encontrarmayor(desordenadas,orden_alfabeto)
+        ordenada.append(agregar)
+        desordenadas.remove(agregar)
+    return ordenada
+
+def main():
+    lista=ordenar_extraterrestre(['miel', 'extraterrestre', 'al', 'automovil', 'auto', 'revestir'],'zyxwvutsrqponmlkjihgfedcba')
+    print(lista)
+main()
+        
+        
+
+
